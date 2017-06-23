cd94fd667c67a691750965f2262af05f062a6f60
prabhsimrandeep singh <psuggal@gmail.com>
2017-06-23 15:24:41 +0530

    Add files via upload

--- comp_fil.m
+++ comp_fil.m
@@ -1,28 +1,19 @@
-function [ grp_del] = comp_fil( alpha,beta,x )
+function [ grp_del ] = comp_fil(beta,alpha, w )
+%s=tf('s');
+%for q=1:0.1:1.5
+ %   for w0=10000:1000:15000
+        %H= (s^2-w0/q*s+w0^2)/(s^2+w0/q*s+w0^2);
+        if(alpha<1)
+            w0=beta/alpha;
+        q=0.001;
+     
+        grp_del=(1/w0)*(2*q*(w.^2)/w0^2+2*q)./(q^2*w.^4/w0^4 + (1-2*q^2)*w.^2/w0^2 + q^2);
 
-
-syms u v
-
-%the equation 
-eqns = [2*(3*u^2-1)/u^3/v^3 + (alpha^3-1)/beta^3 == 0, 2*(5*u^4-5*u^2+1)/(u^5*v^5)+(1-alpha^5)/beta^5 == 0];
-vars = [v u];
-[solv, solu] = solve(eqns, vars);
-
-W=vpa(solv);
-Q=vpa(solu);
-omega=double(W);
-qua_fac=double(Q);
-d=abs(omega)==real(omega);
-e=abs(qua_fac)==real(qua_fac);
-
-w=max(omega(d));
-q=max(qua_fac(e));
-
-
-
-grp_del=1/w*(2*q*(x.^2)/w^2+2*q)./(q^2*x.^4/w^4 + (1-2*q^2)*x.^2/w^2 + q^2);
-
-
+        %figure;
+        %plot(w/w0,grp_del);
+        %xlabel(['w0 = ' num2str(w0) ' q= ' num2str(q)])
+    %end
+%end
 
 end
 
