1f05b18e769137feb3b678642e14559a238065d9
prabhsimrandeep singh <psuggal@gmail.com>
2017-06-23 15:26:01 +0530

    Add files via upload

--- comp_fil.m
+++ comp_fil.m
@@ -1,19 +1,32 @@
-function [ grp_del ] = comp_fil(beta,alpha, w )
-%s=tf('s');
-%for q=1:0.1:1.5
- %   for w0=10000:1000:15000
-        %H= (s^2-w0/q*s+w0^2)/(s^2+w0/q*s+w0^2);
-        if(alpha<1)
-            w0=beta/alpha;
-        q=0.001;
-     
-        grp_del=(1/w0)*(2*q*(w.^2)/w0^2+2*q)./(q^2*w.^4/w0^4 + (1-2*q^2)*w.^2/w0^2 + q^2);
+function [ grp_del] = comp_fil( alpha,beta,w )
 
-        %figure;
-        %plot(w/w0,grp_del);
-        %xlabel(['w0 = ' num2str(w0) ' q= ' num2str(q)])
-    %end
-%end
+
+syms q w0 
+
+%the equation 
+if (alpha>1)
+  eqns = [(((((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4)/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + ((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)^2/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2)*(2*q + (2*beta^2*q)/w0^2))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + (2*q)/(w0^2*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)) + (4*beta*q*((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4))/(w0^2*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2))/w0 + (alpha^3-1)/beta^3 == 0, (((2*q + (2*beta^2*q)/w0^2)*(((((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4)/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + ((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)^2/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2)*((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + (((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)*((((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)*(((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4)/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + ((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)^2/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + (((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)*((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2 - (4*beta*q^2)/(w0^4*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4))))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) - q^2/(w0^4*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)) - (4*beta*q^2*((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4))/(w0^4*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2)))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + (2*q*(((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4)/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + ((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)^2/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2))/(w0^2*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)) + (4*beta*q*((((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)*(((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4)/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + ((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)^2/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4) + (((2*beta*(2*q^2 - 1))/w0^2 - (4*beta^3*q^2)/w0^4)*((2*q^2 - 1)/w0^2 - (6*beta^2*q^2)/w0^4))/(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)^2 - (4*beta*q^2)/(w0^4*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4))))/(w0^2*(q^2 - (beta^2*(2*q^2 - 1))/w0^2 + (beta^4*q^2)/w0^4)))/w0 +(1-alpha^5)/beta^5 == 0];
+else
+  eqns = [];  
+end    
+vars = [q w0];
+[solv, solu] = solve(eqns, vars);
+
+W=vpa(solv);
+Q=vpa(solu);
+omega=double(W);
+qua_fac=double(Q);
+d=abs(omega)==real(omega);
+e=abs(qua_fac)==real(qua_fac);
+
+w0=max(omega(d));
+q=max(qua_fac(e));
+
+
+
+grp_del=(1/w0)*(2*q*(w.^2)/w0^2+2*q)./(q^2*w.^4/w0^4 + (1-2*q^2)*w.^2/w0^2 + q^2);
+
+
 
 end
 
